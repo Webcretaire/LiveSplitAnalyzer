c67cc346fc6d73ce650b492ab0924dcca4c97f9e
Julien EMMANUEl <webcretaire@julien-emmanuel.com>
2022-02-20 02:40:12 +0100

    Make component fully typescript

--- components/SplitsDisplay.vue
+++ components/SplitsDisplay.vue
@@ -1,65 +1,66 @@
-<!-- Please remove this file from your project -->
 <template>
   <div class="p-3">
     <b-form-file
-      @change="previewFiles"
+      @change="fileChange"
       accept=".lss"
       placeholder="Choose a file or drop it here..."
       drop-placeholder="Drop file here..."
       class="mb-3"
     ></b-form-file>
-    <b-row v-if="splitFile">
-      <b-col cols="6" xl="3" lg="4" v-for="split in splits">
+    <b-row v-if="splitFile.length">
+      <b-col cols="6" xl="3" lg="4" v-for="split in splits" :key="split.Name">
         <b-card :title="split.Name" class="text-center mb-3">
-          <img class="mt-3" :alt="split.Name" :src="srcFormattedImage(extractPng(split.Icon))"/>
+          <img class="mt-3" :alt="split.Name" :src="srcFormattedIcon(split)"/>
         </b-card>
       </b-col>
     </b-row>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import {XMLParser}  from 'fast-xml-parser';
 import {extractPng} from '~/util/pngExtractor';
+import {Segment}    from '~/util/splits';
+import {Vue, Component}        from 'nuxt-property-decorator';
 
-export default {
-  name: 'NuxtTutorial',
-  data: () => ({
-    xmlParser: new XMLParser(),
-    splitFile: null
-  }),
-  computed: {
-    parsedSplits() {
-      return this.xmlParser.parse(this.splitFile);
-    },
-    splits() {
-      return this.parsedSplits.Run.Segments.Segment;
-    }
-  },
-  watch: {
-    fileInput(newVal) {
+// See https://github.com/microsoft/TypeScript/issues/31816#issuecomment-593069149
+export type FileEventTarget = EventTarget & { files: FileList };
 
-    }
-  },
-  methods: {
-    srcFormattedImage(rawImage) {
-      return 'data:image/jpeg;base64,' + btoa(
-        new Uint8Array(rawImage)
-          .reduce((data, byte) => data + String.fromCharCode(byte), '')
-      );
-    },
-    previewFiles(ev) {
-      const file = ev.target.files[0];
-      const reader = new FileReader();
+@Component
+export default class SplitsDisplay extends Vue {
+  xmlParser: XMLParser = new XMLParser();
 
-      reader.onload = e => this.splitFile = e.target.result;
-      reader.readAsText(file);
-    },
-    extractPng
-  },
-  mounted() {
+  splitFile: string = '';
+
+  get parsedSplits() {
+    return this.xmlParser.parse(this.splitFile);
   }
-};
+
+  get splits() {
+    return this.parsedSplits.Run.Segments.Segment;
+  }
+
+  srcFormattedIcon(split: Segment): String | null {
+    const png = extractPng(split.Icon);
+    return png ? 'data:image/jpeg;base64,' + btoa(
+      new Uint8Array(png)
+        .reduce((data, byte) => data + String.fromCharCode(byte), '')
+    ) : null;
+  }
+
+  fileChange(ev: Event) {
+    const file   = (<FileEventTarget>ev.target).files[0];
+    const reader = new FileReader();
+
+    reader.onload = e => {
+      if (e.target)
+        this.splitFile = e.target.result as string;
+      else
+        console.error("FileReader error");
+    };
+    reader.readAsText(file);
+  }
+}
 </script>
 
 <style scoped lang="scss">
---
+++ util/splits.ts
@@ -0,0 +1,55 @@
+export interface RealAndGameTime {
+  'RealTime': string,
+  'GameTime': string
+}
+
+export interface AttemptHistory {
+  'Attempt': Array<RealAndGameTime | string>;
+}
+
+export interface RunMetadataVariables {
+  'Variable': string[];
+}
+
+export interface RunMetadata {
+  'Run': string,
+  'Platform': string,
+  'Region': string,
+  'Variables': RunMetadataVariables
+}
+
+export interface SegmentHistory {
+  'Time': Array<RealAndGameTime | string>;
+}
+
+export interface SplitTimes {
+  'SplitTime': RealAndGameTime,
+  'BestSegmentTime': RealAndGameTime,
+  'SegmentHistory': SegmentHistory
+}
+
+export interface Segment {
+  'Name': string,
+  'Icon': string,
+  'SplitTimes': SplitTimes
+}
+
+export interface Segments {
+  Segment: Segment[];
+}
+
+export interface Run {
+  'GameIcon': string,
+  'GameName': string,
+  'CategoryName': string,
+  'LayoutPath': string,
+  'Metadata': RunMetadata,
+  'Offset': string,
+  'AttemptCount': number,
+  'AttemptHistory': AttemptHistory,
+  'Segments': Segments
+}
+
+export interface SplitFile {
+  'Run': Run;
+}

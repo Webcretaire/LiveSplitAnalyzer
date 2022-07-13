b8f30f5bbd90c43c3b06df884b0638e20af7cb95
webhead3893 <71961459+webhead3893@users.noreply.github.com>
2022-07-13 22:51:19 +0800

    Added game cover image to run overview

--- components/RunOverview.vue
+++ components/RunOverview.vue
@@ -1,5 +1,6 @@
 <template>
   <collapsible-card id="RunOverviewCard" class="text-center" :title="run.GameName + ' - ' + run.CategoryName" starts-open>
+    <img :src="gameCover" class="mt-1 mb-3">
     <p>{{ run.AttemptCount }} attempts</p>
     <p class="m-0"><strong>Finished runs:</strong> {{ finishedRuns }}</p>
     <p class="m-0"><strong>Number of PBs:</strong> {{ PBs.length }}</p>
@@ -13,7 +14,7 @@
 </template>
 
 <script lang="ts">
-import {Vue, Component, Prop} from 'nuxt-property-decorator';
+import {Vue, Component, Prop, Watch} from 'nuxt-property-decorator';
 import {Run, selectTime}      from '~/util/splits';
 import {stringTimeToSeconds}  from '~/util/durations';
 
@@ -21,6 +22,8 @@ import {stringTimeToSeconds}  from '~/util/durations';
 export default class RunOverview extends Vue {
   @Prop()
   run!: Run;
+
+  gameCover: string = '';
 
   get finishedRuns() {
     return this.run.AttemptHistory.Attempt.filter(a => selectTime(a)).length;
@@ -45,6 +48,22 @@ export default class RunOverview extends Vue {
     return this.run.Metadata.Variables?.Variable;
   }
 
+  @Watch('run.GameName', {immediate: true})
+  coverSource() {
+    const url = `https://www.speedrun.com/api/v1/games?name=${encodeURIComponent(this.run.GameName)}`;
+    console.log('ejjhfhjej')
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.gameCover = data.data[0].assets['cover-small'].uri);
+  }
+
   visible: boolean = true;
 }
 </script>
+
+<style scoped lang="scss">
+img {
+  max-height: 20vh;
+  filter: drop-shadow(0 0 0.5rem rgba(0, 0, 0, 1));
+}
+</style>
